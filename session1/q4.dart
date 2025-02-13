b3d3590f54a591a1a1326a5098db5b95d507fd34
Mostafa Morgan <ahmedmorgan290513@yahoo.com>
2025-02-13 17:03:59 +0200

    q4.dart

---
+++ session1/q4.dart
@@ -0,0 +1,14 @@
+//- Create a map named person with the keys and values: 'name': 'Alice', 'age': '25', and 'city': 'New
+//York'.
+//- Print the age of the person from the map.
+//- Update the city to 'Los Angeles' and print the updated map.
+void main() {
+  Map<String, String> person = {
+    'name': 'Alice',
+    'age': '25',
+    'city': 'New York',
+  };
+  print('Age: ${person['age']}');
+  person['city'] = 'Los Angeles';
+  print('Updated map: $person');
+}
