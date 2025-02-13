bd4f009241cc73adc702c57dd43aeab4d11b5800
Mostafa Morgan <ahmedmorgan290513@yahoo.com>
2025-02-13 16:22:21 +0200

    q1.dart

--- session1/q1.dart
+++ session1/q1.dart
@@ -1,3 +1,8 @@
+//- Create a string variable greeting with the value 'Hello, Dart!'.
+//- Print the length of greeting.
+//- Convert greeting to uppercase and print it.
 void main() {
-  print('hello World');
+  String greeting = 'hallo dart';
+  print(greeting.length);
+  print(greeting.toUpperCase());
 }
